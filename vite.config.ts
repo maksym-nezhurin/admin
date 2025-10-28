293b4a601e763d4899b1f1f5b32e053246899934
Maksym Nezhurin <mnezhurin17@gmail.com>
2025-10-28 09:08:03 +0100

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   define: {
     'process.env.API_BASE_URL': JSON.stringify(process.env.API_BASE_URL),
   },
+  build: {
+    rollupOptions: {
+      external: ['embla-carousel-react']
+    }
+  },
   server: {
     port: 5173,
     host: '0.0.0.0',
