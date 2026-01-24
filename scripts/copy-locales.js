8b5711534d1ba70a8e95ac63305bcc039e049ed8
Maksym Nezhurin <mnezhurin17@gmail.com>
2026-01-24 12:23:23 +0100

    new changes

--- scripts/copy-locales.js
+++ scripts/copy-locales.js
@@ -17,10 +17,10 @@ console.log('📁 [copy-locales] Script directory:', __dirname);
 
 // Paths to check (in order of preference)
 const possibleSourcePaths = [
+  // From root node_modules (most common in monorepo)
+  join(__dirname, '../../../node_modules/@reelo/i18n/locales'),
   // From admin app node_modules (symlink)
   join(__dirname, '../../node_modules/@reelo/i18n/locales'),
-  // From root node_modules
-  join(__dirname, '../../../node_modules/@reelo/i18n/locales'),
   // From admin app directory
   join(__dirname, '../node_modules/@reelo/i18n/locales'),
 ];
